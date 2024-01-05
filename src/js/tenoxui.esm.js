1c4711c319375daa5e0813dd692d3d6597c5f94e
nousantx <nousantx@gmail.com>
2024-01-05 08:27:05 +0700

    Update Source

--- src/js/tenoxui.esm.js
+++ src/js/tenoxui.esm.js
@@ -1,5 +1,5 @@
 /*!
- * TenoxUI CSS Framework v0.4.21 [ https://tenoxui.web.app ]
+ * TenoxUI CSS Framework v0.4.23 [ https://tenoxui.web.app ]
  * copyright (c) 2024 nousantx
  * licensed under MIT [ https://github.com/nousantx/tenoxui/blob/main/LICENSE ]
  */
--- src/ts/tenoxui.ts
+++ src/ts/tenoxui.ts
@@ -3,7 +3,7 @@ interface PropertyMap {
 }
 
 /*!
- * TenoxUI CSS Framework v0.4.21 [ https://tenoxui.web.app ]
+ * TenoxUI CSS Framework v0.4.23 [ https://tenoxui.web.app ]
  * copyright (c) 2024 nousantx
  * licensed under MIT [ https://github.com/nousantx/tenoxui/blob/main/LICENSE ]
  */
