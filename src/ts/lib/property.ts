550ba7dde3bbaf6944ebddf116da3ed083e29d6c
nousantx <nousantx@gmail.com>
2024-05-09 08:51:42 +0700

    chore(ts): v0.9.0-alpha.1 code

--- src/ts/lib/property.ts
+++ src/ts/lib/property.ts
@@ -1,9 +1,3 @@
-/*!
- * tenoxui/css v0.8.0 (https://github.com/tenoxui/css)
- * Copyright (c) 2024 NOuSantx
- * Licensed under the MIT License (https://github.com/tenoxui/css/blob/main/LICENSE)
- */
-
 interface PropertyMap {
   [key: string]: string | string[];
 }
