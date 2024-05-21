85f0a4fac816ce4edffa8c675f48db06d91a2c7d
nousantx <112807896+nousantx@users.noreply.github.com>
2024-05-21 19:28:42 +0700

    test: update valueValidator.test.js

--- test/valueValidator.test.js
+++ test/valueValidator.test.js
@@ -7,7 +7,7 @@
 function testClassName(className) {
   // Regular expression for handling all possible values
   const match = className.match(
-    /([a-zA-Z]+(?:-[a-zA-Z]+)*)-(-?(?:\d+(\.\d+)?)|(?:[a-zA-Z]+(?:-[a-zA-Z]+)*(?:-[a-zA-Z]+)*)|(?:#[0-9a-fA-F]+)|(?:\[[^\]]+\])|(?:\{[^\}]+\}))([a-zA-Z%]*)/
+    /(?:([a-z-]+):)?(-?[a-zA-Z0-9_]+(?:-[a-zA-Z0-9_]+)*|\[--[a-zA-Z0-9_-]+\])-(-?(?:\d+(\.\d+)?)|(?:[a-zA-Z0-9_]+(?:-[a-zA-Z0-9_]+)*(?:-[a-zA-Z0-9_]+)*)|(?:#[0-9a-fA-F]+)|(?:\[[^\]]+\])|(?:\{[^\}]+\})|(?:\$[^\s]+))([a-zA-Z%]*)/
   );
 
   // Error handling for invalid class names
@@ -15,21 +15,35 @@ function testClassName(className) {
     console.error(`❌ ${className} does not match the required pattern`);
     return false;
   }
-
   // Extracted components from the class name
-  const type = match[1]; // Property class (e.g., p-, m-, flex-, fx-, filter-, etc.)
-  const value = match[2]; // Possible value (e.g., 10, red, blue, etc.)
-  const unitOrValue = match[4] || "unset"; // Possible unit (e.g., px, rem, em, s, %) or value if no unit provided.
+  const breakpoint = match[1]; // match breakpoints
+  const type = match[2]; // Property class (e.g., p-, m-, flex-, fx-, filter-, etc.)
+  const value = match[3]; // Possible value (e.g., 10, red, blue, etc.)
+  const unitOrValue = match[5] || "unset"; // Possible unit (e.g., px, rem, em, s, %) or value if no unit provided.
+
+  const vb = breakpoint
+    ? `✅ Breakpoint: "${breakpoint}", Type: "${type}", Value: "${value}", Unit: "${unitOrValue}"`
+    : `✅ Type: "${type}", Value: "${value}", Unit: "${unitOrValue}"`;
+
+  // logginh the values
+  console.log(vb);
 
   // Log the extracted components if the class name is valid
-  console.log(`✅ Type: ${type}, Value: ${value}, Unit: ${unitOrValue}`);
   return true;
 }
 
 // Example usage
 testClassName("tc-#0d0d0d");
 
-const classNames = ["filter-[blur(10px)]", "p-[--padding]", "p-10px", "m-20%"];
-classNames.forEach((className) => {
+const classNames = [
+  "filter-[blur(10px)]",
+  "p-[--padding]",
+  "p-10px",
+  "m-20%",
+  "sm:bg-blue",
+  "[--tenox]-blue",
+  "[--bg-opa]-1"
+];
+classNames.forEach(className => {
   testClassName(className);
 });
