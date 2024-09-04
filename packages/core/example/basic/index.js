3d5af10c0b60d2f984163e87c727c1003345f228
nousantx <nousantx@gmail.com>
2024-09-05 00:01:06 +0700

    feat: remove current examples directory from core packages

--- packages/core/example/basic/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const selector = document.querySelector(".my-element");
-  const styler = new makeTenoxUI({
-    element: selector,
-    property: { bg: "background", text: "color" }
-  });
-
-  styler.applyMultiStyles("bg-blue text-red");
-});
