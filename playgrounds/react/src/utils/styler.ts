e970ab74278c321c008e09387bac215a32b9f4a5
nousantx <nousantx@gmail.com>
2024-09-18 19:55:43 +0700

    fix(playground:react): path to @tenoxui/property error

--- playgrounds/react/src/utils/styler.ts
+++ playgrounds/react/src/utils/styler.ts
@@ -1,12 +1,14 @@
 import { useLayoutEffect } from "react";
 import { makeTenoxUI } from "@tenoxui/core";
-import { defaultProps as txProps } from "@tenoxui/property";
+import { defaultProps as txProps } from "@tenoxui/property/dist/esm/default.js";
+
+// const txProps = { p: "padding" };
 
 export function styler(property?) {
   // Using the `useLayoutEffect` hook to ensure the effect runs immediately after DOM mutations 😜
   useLayoutEffect(() => {
     // Query all elements that have a `class` attribute
-    document.querySelectorAll("*[class]").forEach((element) => {
+    document.querySelectorAll("*[class]").forEach(element => {
       // Create new instance for every elements
       const tenoxui = new makeTenoxUI({ element, property: { ...txProps, ...property } });
 
