d3ace0c4445868b9e15b62026acee13f6b887187
nousantx <nousantx@gmail.com>
2024-10-13 09:18:10 +0700

    feat(property): update since a while :)

--- packages/@tenoxui-property/src/umd/default.js
+++ packages/@tenoxui-property/src/umd/default.js
@@ -1,114 +1,114 @@
-((root, factory) => {
-  if (typeof define === "function" && define.amd) {
-    define([], factory);
-  } else if (typeof exports === "object" && typeof module === "object") {
-    module.exports = factory();
-  } else if (typeof exports === "object") {
-    exports.property = factory().property;
+;((root, factory) => {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory)
+  } else if (typeof exports === 'object' && typeof module === 'object') {
+    module.exports = factory()
+  } else if (typeof exports === 'object') {
+    exports.property = factory().property
   } else {
-    root.property = factory().property;
+    root.property = factory().property
   }
-})(typeof self !== "undefined" ? self : this, () => {
+})(typeof self !== 'undefined' ? self : this, () => {
   const property = {
     // padding
-    p: "padding",
-    pt: "paddingTop",
-    pb: "paddingBottom",
-    pr: "paddingRight",
-    pl: "paddingLeft",
-    py: ["paddingTop", "paddingBottom"],
-    px: ["paddingLeft", "paddingRight"],
+    p: 'padding',
+    pt: 'paddingTop',
+    pb: 'paddingBottom',
+    pr: 'paddingRight',
+    pl: 'paddingLeft',
+    py: ['paddingTop', 'paddingBottom'],
+    px: ['paddingLeft', 'paddingRight'],
     // margin
-    m: "margin",
-    mt: "marginTop",
-    mb: "marginBottom",
-    mr: "marginRight",
-    ml: "marginLeft",
-    my: ["marginTop", "marginBottom"],
-    mx: ["marginLeft", "marginRight"],
+    m: 'margin',
+    mt: 'marginTop',
+    mb: 'marginBottom',
+    mr: 'marginRight',
+    ml: 'marginLeft',
+    my: ['marginTop', 'marginBottom'],
+    mx: ['marginLeft', 'marginRight'],
     // font and text style
-    font: "font",
-    fs: "fontSize",
-    fw: "fontWeight",
-    lh: "lineHeight",
-    ls: "letterSpacing",
-    ta: "textAlign",
-    text: "color",
-    td: "textDecoration",
-    ti: "textIndent",
-    tn: "textTransform",
-    tw: "textWrap",
-    ws: "wordSpacing",
-    family: "fontFamily",
-    "font-s": "fontStyle",
-    "white-space": "whiteSpace",
+    font: 'font',
+    fs: 'fontSize',
+    fw: 'fontWeight',
+    lh: 'lineHeight',
+    ls: 'letterSpacing',
+    ta: 'textAlign',
+    text: 'color',
+    td: 'textDecoration',
+    ti: 'textIndent',
+    tn: 'textTransform',
+    tw: 'textWrap',
+    ws: 'wordSpacing',
+    family: 'fontFamily',
+    'font-s': 'fontStyle',
+    'white-space': 'whiteSpace',
     // positioning
-    pn: "position",
-    z: "zIndex",
-    t: "top",
-    b: "bottom",
-    r: "right",
-    l: "left",
+    pn: 'position',
+    z: 'zIndex',
+    t: 'top',
+    b: 'bottom',
+    r: 'right',
+    l: 'left',
     // display
-    d: "display",
+    d: 'display',
     // size
-    w: "width",
-    "w-mx": "maxWidth",
-    "w-mn": "minWidth",
-    h: "height",
-    "h-mx": "maxHeight",
-    "h-mn": "minHeight",
+    w: 'width',
+    'w-mx': 'maxWidth',
+    'w-mn': 'minWidth',
+    h: 'height',
+    'h-mx': 'maxHeight',
+    'h-mn': 'minHeight',
     // background
-    bg: "background",
-    "bg-c": "backgroundColor",
-    "bg-img": "backgroundImage",
-    "bg-size": "backgroundSize",
-    "bg-r": "backgroundRepeat",
-    "bg-pn": "backgroundPosition",
+    bg: 'background',
+    'bg-c': 'backgroundColor',
+    'bg-img': 'backgroundImage',
+    'bg-size': 'backgroundSize',
+    'bg-r': 'backgroundRepeat',
+    'bg-pn': 'backgroundPosition',
     // flex
-    flex: "flex",
-    "flex-d": "flexDirection",
-    "flex-w": "flexWrap",
-    "flex-b": "flexBasis",
-    "flex-g": "flexGrow",
-    "flex-s": "flexShrink",
+    flex: 'flex',
+    'flex-d': 'flexDirection',
+    'flex-w': 'flexWrap',
+    'flex-b': 'flexBasis',
+    'flex-g': 'flexGrow',
+    'flex-s': 'flexShrink',
     // gap
-    gap: "gap",
-    "gap-y": "columnGap",
-    "gap-x": "rowGap",
+    gap: 'gap',
+    'gap-y': 'columnGap',
+    'gap-x': 'rowGap',
     // align
-    "align-i": "alignItems",
+    'align-i': 'alignItems',
     // justify
-    "justify-c": "justifyContent",
+    'justify-c': 'justifyContent',
     // border
-    bdr: "border",
-    "bdr-c": "borderColor",
-    "bdr-r": "borderRadius",
-    "bdr-s": "borderStyle",
-    "bdr-w": "borderWidth",
-    "bdr-w-l": "borderLeftWidth",
-    "bdr-w-r": "borderRightWidth",
-    "bdr-w-t": "borderTopWidth",
-    "bdr-w-b": "borderBottomWidth",
+    bdr: 'border',
+    'bdr-c': 'borderColor',
+    'bdr-r': 'borderRadius',
+    'bdr-s': 'borderStyle',
+    'bdr-w': 'borderWidth',
+    'bdr-w-l': 'borderLeftWidth',
+    'bdr-w-r': 'borderRightWidth',
+    'bdr-w-t': 'borderTopWidth',
+    'bdr-w-b': 'borderBottomWidth',
     // cursor
-    cursor: "cursor",
+    cursor: 'cursor',
     // overflow
-    over: "overflow",
-    "over-y": "overflowY",
-    "over-x": "overflowX",
+    over: 'overflow',
+    'over-y': 'overflowY',
+    'over-x': 'overflowX',
     // aspect Ratio
-    ratio: "aspectRatio",
+    ratio: 'aspectRatio',
     // transition
-    transition: "transition",
-    "tr-time": "transitionDuration",
-    "tr-prop": "transitionProperty",
-    "tr-timing": "transitionTimingFunction",
-    "tr-delay": "transitionDelay",
+    transition: 'transition',
+    'tr-time': 'transitionDuration',
+    'tr-prop': 'transitionProperty',
+    'tr-timing': 'transitionTimingFunction',
+    'tr-delay': 'transitionDelay',
     // more
-    shadow: "boxShadow",
+    shadow: 'boxShadow',
     // custom property
-    box: ["width", "height"],
-    "flex-parent": ["justifyContent", "alignItems"],
-  };
-  return { property };
-});
+    box: ['width', 'height'],
+    'flex-parent': ['justifyContent', 'alignItems']
+  }
+  return { property }
+})
--- packages/@tenoxui-property/src/umd/full.js
+++ packages/@tenoxui-property/src/umd/full.js
@@ -1,180 +1,180 @@
-((root, factory) => {
-  if (typeof define === "function" && define.amd) {
-    define([], factory);
-  } else if (typeof exports === "object" && typeof module === "object") {
-    module.exports = factory();
-  } else if (typeof exports === "object") {
-    exports.property = factory().property;
+;((root, factory) => {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory)
+  } else if (typeof exports === 'object' && typeof module === 'object') {
+    module.exports = factory()
+  } else if (typeof exports === 'object') {
+    exports.property = factory().property
   } else {
-    root.property = factory().property;
+    root.property = factory().property
   }
-})(typeof self !== "undefined" ? self : this, () => {
+})(typeof self !== 'undefined' ? self : this, () => {
   const property = {
     // padding
-    p: "padding",
-    pt: "paddingTop",
-    pb: "paddingBottom",
-    pr: "paddingRight",
-    pl: "paddingLeft",
-    py: ["paddingTop", "paddingBottom"],
-    px: ["paddingLeft", "paddingRight"],
+    p: 'padding',
+    pt: 'paddingTop',
+    pb: 'paddingBottom',
+    pr: 'paddingRight',
+    pl: 'paddingLeft',
+    py: ['paddingTop', 'paddingBottom'],
+    px: ['paddingLeft', 'paddingRight'],
     // margin
-    m: "margin",
-    mt: "marginTop",
-    mb: "marginBottom",
-    mr: "marginRight",
-    ml: "marginLeft",
-    my: ["marginTop", "marginBottom"],
-    mx: ["marginLeft", "marginRight"],
+    m: 'margin',
+    mt: 'marginTop',
+    mb: 'marginBottom',
+    mr: 'marginRight',
+    ml: 'marginLeft',
+    my: ['marginTop', 'marginBottom'],
+    mx: ['marginLeft', 'marginRight'],
     // font and text style
-    font: "font",
-    fs: "fontSize",
-    fw: "fontWeight",
-    lh: "lineHeight",
-    ls: "letterSpacing",
-    ta: "textAlign",
-    text: "color",
-    td: "textDecoration",
-    ti: "textIndent",
-    tn: "textTransform",
-    tw: "textWrap",
-    ws: "wordSpacing",
-    family: "fontFamily",
-    "font-s": "fontStyle",
-    "white-space": "whiteSpace",
+    font: 'font',
+    fs: 'fontSize',
+    fw: 'fontWeight',
+    lh: 'lineHeight',
+    ls: 'letterSpacing',
+    ta: 'textAlign',
+    text: 'color',
+    td: 'textDecoration',
+    ti: 'textIndent',
+    tn: 'textTransform',
+    tw: 'textWrap',
+    ws: 'wordSpacing',
+    family: 'fontFamily',
+    'font-s': 'fontStyle',
+    'white-space': 'whiteSpace',
     // positioning
-    pn: "position",
-    z: "zIndex",
-    t: "top",
-    b: "bottom",
-    r: "right",
-    l: "left",
+    pn: 'position',
+    z: 'zIndex',
+    t: 'top',
+    b: 'bottom',
+    r: 'right',
+    l: 'left',
     // display
-    d: "display",
+    d: 'display',
     // size
-    w: "width",
-    "w-mx": "maxWidth",
-    "w-mn": "minWidth",
-    h: "height",
-    "h-mx": "maxHeight",
-    "h-mn": "minHeight",
+    w: 'width',
+    'w-mx': 'maxWidth',
+    'w-mn': 'minWidth',
+    h: 'height',
+    'h-mx': 'maxHeight',
+    'h-mn': 'minHeight',
     // background
-    bg: "background",
-    "bg-c": "backgroundColor",
-    "bg-img": "backgroundImage",
-    "bg-size": "backgroundSize",
-    "bg-r": "backgroundRepeat",
-    "bg-pn": "backgroundPosition",
+    bg: 'background',
+    'bg-c': 'backgroundColor',
+    'bg-img': 'backgroundImage',
+    'bg-size': 'backgroundSize',
+    'bg-r': 'backgroundRepeat',
+    'bg-pn': 'backgroundPosition',
     // flex
-    flex: "flex",
-    "flex-d": "flexDirection",
-    "flex-w": "flexWrap",
-    "flex-b": "flexBasis",
-    "flex-g": "flexGrow",
-    "flex-s": "flexShrink",
+    flex: 'flex',
+    'flex-d': 'flexDirection',
+    'flex-w': 'flexWrap',
+    'flex-b': 'flexBasis',
+    'flex-g': 'flexGrow',
+    'flex-s': 'flexShrink',
     // gap
-    gap: "gap",
-    "gap-y": "columnGap",
-    "gap-x": "rowGap",
+    gap: 'gap',
+    'gap-y': 'columnGap',
+    'gap-x': 'rowGap',
     // align
-    "align-i": "alignItems",
+    'align-i': 'alignItems',
     // justify
-    "justify-c": "justifyContent",
+    'justify-c': 'justifyContent',
     // border
-    bdr: "border",
-    "bdr-c": "borderColor",
-    "bdr-r": "borderRadius",
-    "bdr-s": "borderStyle",
-    "bdr-w": "borderWidth",
-    "bdr-w-l": "borderLeftWidth",
-    "bdr-w-r": "borderRightWidth",
-    "bdr-w-t": "borderTopWidth",
-    "bdr-w-b": "borderBottomWidth",
+    bdr: 'border',
+    'bdr-c': 'borderColor',
+    'bdr-r': 'borderRadius',
+    'bdr-s': 'borderStyle',
+    'bdr-w': 'borderWidth',
+    'bdr-w-l': 'borderLeftWidth',
+    'bdr-w-r': 'borderRightWidth',
+    'bdr-w-t': 'borderTopWidth',
+    'bdr-w-b': 'borderBottomWidth',
     // cursor
-    cursor: "cursor",
+    cursor: 'cursor',
     // overflow
-    over: "overflow",
-    "over-y": "overflowY",
-    "over-x": "overflowX",
+    over: 'overflow',
+    'over-y': 'overflowY',
+    'over-x': 'overflowX',
     // aspect Ratio
-    ratio: "aspectRatio",
+    ratio: 'aspectRatio',
     // transition
-    transition: "transition",
-    "tr-time": "transitionDuration",
-    "tr-prop": "transitionProperty",
-    "tr-timing": "transitionTimingFunction",
-    "tr-delay": "transitionDelay",
+    transition: 'transition',
+    'tr-time': 'transitionDuration',
+    'tr-prop': 'transitionProperty',
+    'tr-timing': 'transitionTimingFunction',
+    'tr-delay': 'transitionDelay',
     // more
-    shadow: "boxShadow",
+    shadow: 'boxShadow',
     // custom property
-    box: ["width", "height"],
-    "flex-parent": ["justifyContent", "alignItems"],
+    box: ['width', 'height'],
+    'flex-parent': ['justifyContent', 'alignItems'],
 
     // full properties
-    all: "all",
-    order: "order",
-    visibility: "visibility",
-    resize: "resize",
-    float: "float",
-    isolation: "isolation",
-    col: "columns",
-    "box-si": "boxSizing",
+    all: 'all',
+    order: 'order',
+    visibility: 'visibility',
+    resize: 'resize',
+    float: 'float',
+    isolation: 'isolation',
+    col: 'columns',
+    'box-si': 'boxSizing',
     // place like
-    "pc-i": "placeItems",
-    "pc-c": "placeContents",
-    "pc-s": "placeSelf",
+    'pc-i': 'placeItems',
+    'pc-c': 'placeContents',
+    'pc-s': 'placeSelf',
 
     // border extended
-    "bg-att": "backgroundAttachment",
-    "bg-ori": "backgroundOrigin",
-    "bg-clip": "backgroundClip",
-    "bg-p-x": "backgroundPositionX",
-    "bg-p-y": "backgroundPositionY",
-    "bg-r-x": "backgroundRepeatX",
-    "bg-r-y": "backgroundRepeaty",
+    'bg-att': 'backgroundAttachment',
+    'bg-ori': 'backgroundOrigin',
+    'bg-clip': 'backgroundClip',
+    'bg-p-x': 'backgroundPositionX',
+    'bg-p-y': 'backgroundPositionY',
+    'bg-r-x': 'backgroundRepeatX',
+    'bg-r-y': 'backgroundRepeaty',
     // align and justify
-    "align-c": "alignContent",
-    "align-s": "alignSelf",
-    "justify-i": "justifyItems",
-    "justify-s": "justifySelf",
+    'align-c': 'alignContent',
+    'align-s': 'alignSelf',
+    'justify-i': 'justifyItems',
+    'justify-s': 'justifySelf',
     // border color
-    "bdr-c-t": "borderTopColor",
-    "bdr-c-b": "borderBottomColor",
-    "bdr-c-l": "borderLeftColor",
-    "bdr-c-r": "borderRightColor",
+    'bdr-c-t': 'borderTopColor',
+    'bdr-c-b': 'borderBottomColor',
+    'bdr-c-l': 'borderLeftColor',
+    'bdr-c-r': 'borderRightColor',
     // border style
-    "bdr-s-t": "borderTopStyle",
-    "bdr-s-b": "borderBottomStyle",
-    "bdr-s-l": "borderLeftStyle",
-    "bdr-s-r": "borderRightStyle",
+    'bdr-s-t': 'borderTopStyle',
+    'bdr-s-b': 'borderBottomStyle',
+    'bdr-s-l': 'borderLeftStyle',
+    'bdr-s-r': 'borderRightStyle',
     // border Radius
-    "bdr-r-t": ["borderTopRightRadius", "borderTopLeftRadius"],
-    "bdr-r-b": ["borderBottomRightRadius", "borderBottomLeftRadius"],
-    "bdr-r-l": ["borderBottomLeftRadius", "borderTopLeftRadius"],
-    "bdr-r-r": ["borderBottomRightRadius", "borderTopRightRadius"],
-    "bdr-r-tl": "borderTopLeftRadius",
-    "bdr-r-tr": "borderTopRightRadius",
-    "bdr-r-bl": "borderBottomLeftRadius",
-    "bdr-r-br": "borderBottomRightRadius",
+    'bdr-r-t': ['borderTopRightRadius', 'borderTopLeftRadius'],
+    'bdr-r-b': ['borderBottomRightRadius', 'borderBottomLeftRadius'],
+    'bdr-r-l': ['borderBottomLeftRadius', 'borderTopLeftRadius'],
+    'bdr-r-r': ['borderBottomRightRadius', 'borderTopRightRadius'],
+    'bdr-r-tl': 'borderTopLeftRadius',
+    'bdr-r-tr': 'borderTopRightRadius',
+    'bdr-r-bl': 'borderBottomLeftRadius',
+    'bdr-r-br': 'borderBottomRightRadius',
     // other border
-    "bdr-img": "borderImage",
+    'bdr-img': 'borderImage',
     // list style
-    "list-s": "listStyle",
-    "list-s-img": "listStyleImage",
-    "list-s-pn": "listStylePosition",
-    "list-s-type": "listStyleType",
+    'list-s': 'listStyle',
+    'list-s-img': 'listStyleImage',
+    'list-s-pn': 'listStylePosition',
+    'list-s-type': 'listStyleType',
     // animation
-    animation: "animation",
-    "an-name": "animationName",
-    "an-direction": "animationDirection",
-    "an-fill-mode": "animationFillMode",
-    "an-play-state": "animationPlayState",
-    "an-time": "animationDuration",
-    "an-timing": "animationTimingFunction",
-    "an-delay": "animationDelay",
+    animation: 'animation',
+    'an-name': 'animationName',
+    'an-direction': 'animationDirection',
+    'an-fill-mode': 'animationFillMode',
+    'an-play-state': 'animationPlayState',
+    'an-time': 'animationDuration',
+    'an-timing': 'animationTimingFunction',
+    'an-delay': 'animationDelay',
     // transform
     transform: {
-      property: "transform",
+      property: 'transform',
       value: `rotate(var(--tx_rotate, 0))
 rotateY(var(--tx_rotate-y, 0))
 rotateX(var(--tx_rotate-x, 0))
@@ -189,26 +189,26 @@ skewX(var(--tx_skew-x, 0))
 translate(var(--tx_translate, 0))
 translateY(var(--tx_translate-y, 0))
 translateX(var(--tx_translate-x, 0))
-translateZ(var(--tx_translate-z, 0))`,
+translateZ(var(--tx_translate-z, 0))`
     },
-    rotate: "--tx_rotate",
-    "rotate-y": "--tx_rotate-y",
-    "rotate-x": "--tx_rotate-x",
-    "rotate-z": "--tx_rotate-z",
-    scale: "--tx_scale",
-    "scale-x": "--tx_scale-x",
-    "scale-y": "--tx_scale-y",
-    "scale-z": "--tx_scale-z",
-    skew: "--tx_skew",
-    "skew-x": "--tx_skew-x",
-    "skew-y": "--tx_skew-y",
-    translate: "--tx_translate",
-    "translate-x": "--tx_translate-x",
-    "translate-y": "--tx_translate-y",
-    "translate-z": "--tx_translate-z",
+    rotate: '--tx_rotate',
+    'rotate-y': '--tx_rotate-y',
+    'rotate-x': '--tx_rotate-x',
+    'rotate-z': '--tx_rotate-z',
+    scale: '--tx_scale',
+    'scale-x': '--tx_scale-x',
+    'scale-y': '--tx_scale-y',
+    'scale-z': '--tx_scale-z',
+    skew: '--tx_skew',
+    'skew-x': '--tx_skew-x',
+    'skew-y': '--tx_skew-y',
+    translate: '--tx_translate',
+    'translate-x': '--tx_translate-x',
+    'translate-y': '--tx_translate-y',
+    'translate-z': '--tx_translate-z',
     // filter
     filter: {
-      property: "filter",
+      property: 'filter',
       value: `blur(var(--tx_blur, 0)) 
 brightness(var(--tx_brightness, 1)) 
 contrast(var(--tx_contrast, 1)) 
@@ -218,20 +218,20 @@ invert(var(--tx_invert, 0))
 opacity(var(--tx_opacity, 1)) 
 saturate(var(--tx_saturate, 1)) 
 sepia(var(--tx_sepia, 0))
-drop-shadow(var(--tx_drop-shadow, 0 0 0 rgb(0 0 0 / 0)))`,
+drop-shadow(var(--tx_drop-shadow, 0 0 0 rgb(0 0 0 / 0)))`
     },
-    blur: "--tx_blur",
-    brightness: "--tx_brightness",
-    contrast: "--tx_contrast",
-    grayscale: "--tx_grayscale",
-    "hue-rotate": "--tx_hue-rotate",
-    invert: "--tx_invert",
-    opacity: "--tx_opacity",
-    saturate: "--tx_saturate",
-    sepia: "--tx_sepia",
+    blur: '--tx_blur',
+    brightness: '--tx_brightness',
+    contrast: '--tx_contrast',
+    grayscale: '--tx_grayscale',
+    'hue-rotate': '--tx_hue-rotate',
+    invert: '--tx_invert',
+    opacity: '--tx_opacity',
+    saturate: '--tx_saturate',
+    sepia: '--tx_sepia',
     // backdrop-filter
     backdrop: {
-      property: "backdropFilter",
+      property: 'backdropFilter',
       value: `blur(var(--back_blur, 0)) 
 brightness(var(--back_brightness, 1)) 
 contrast(var(--back_contrast, 1)) 
@@ -240,17 +240,17 @@ hue-rotate(var(--back_hue-rotate, 0))
 invert(var(--back_invert, 0)) 
 opacity(var(--back_opacity, 1)) 
 saturate(var(--back_saturate, 1)) 
-sepia(var(--back_sepia, 0))`,
+sepia(var(--back_sepia, 0))`
     },
-    "back-blur": "--back_blur",
-    "back-brightness": "--back_brightness",
-    "back-contrast": "--back_contrast",
-    "back-grayscale": "--back_grayscale",
-    "back-hue-rotate": "--back_hue-rotate",
-    "back-invert": "--back_invert",
-    "back-opacity": "--back_opacity",
-    "back-saturate": "--back_saturate",
-    "back-sepia": "--back_sepia",
-  };
-  return { property };
-});
+    'back-blur': '--back_blur',
+    'back-brightness': '--back_brightness',
+    'back-contrast': '--back_contrast',
+    'back-grayscale': '--back_grayscale',
+    'back-hue-rotate': '--back_hue-rotate',
+    'back-invert': '--back_invert',
+    'back-opacity': '--back_opacity',
+    'back-saturate': '--back_saturate',
+    'back-sepia': '--back_sepia'
+  }
+  return { property }
+})
