41751dd33f0b116b66b213da471ce9a1b71b90fd
nousantx <nousantx@gmail.com>
2024-12-01 04:57:35 +0700

    feat(playground): remove react app

--- playgrounds/react/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { Routes } from "@generouted/react-router";
-import "./src/styles/css/index.css";
-import "remixicon/fonts/remixicon.css";
-
-createRoot(document.getElementById("root")!).render(<Routes />);
--- playgrounds/react/src/styles/properties.ts
+++
@@ -1,4 +0,0 @@
-export const properties = {
-  all: "all",
-  "bg-opacity": "--bg-opa",
-};
