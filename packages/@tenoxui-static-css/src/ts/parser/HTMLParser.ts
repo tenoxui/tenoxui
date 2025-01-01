8b8ea0dee84acb23dbbf9ec3e0d6f8408cd566f8
nousantx <nousantx@gmail.com>
2025-01-01 17:43:41 +0700

    remove: static-css package

--- packages/@tenoxui-static-css/src/ts/parser/HTMLParser.ts
+++
@@ -1,8 +0,0 @@
-import { BaseParser } from './base'
-
-export class HTMLParser extends BaseParser {
-  parse(content: string): string[] {
-    // const root = parse(content)
-    return Array.from(this.extractClassNames(content))
-  }
-}
