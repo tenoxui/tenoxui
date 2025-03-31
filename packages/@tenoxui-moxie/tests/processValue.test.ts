7b3353318e3a5f8b633296a7702273ab632605e1
nousantx <nousantx@gmail.com>
2025-03-31 19:01:05 +0700

    test(moxie-core): add new tests

--- packages/@tenoxui-moxie/tests/processValue.test.ts
+++ packages/@tenoxui-moxie/tests/processValue.test.ts
@@ -24,6 +24,8 @@ describe('Value Processor', () => {
     expect(ui.processValue('(20px)')).toBe('20px')
     expect(ui.processValue('[20px]')).toBe('20px')
     expect(ui.processValue('(rgb(255_0_0))')).toBe('rgb(255 0 0)')
+    expect(ui.processValue('[hello_world]')).toBe('hello world')
+    expect(ui.processValue('[hello\\_world]')).toBe('hello_world')
     // parse value from this.values
     expect(ui.processValue('2xl')).toBe('48rem') // no group inputted
     expect(ui.processValue('2xl', '', 'container')).toBe('28rem') // use container group
