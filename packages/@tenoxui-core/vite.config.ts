23032a2e91ec09b5bdeb40fda9aca3ba662829c2
nousantx <nousantx@gmail.com>
2025-04-29 18:17:44 +0700

    feat(core): update vite config

--- packages/@tenoxui-core/vite.config.ts
+++ packages/@tenoxui-core/vite.config.ts
@@ -3,15 +3,5 @@ import { createConfig } from '../vite.config.base.ts'
 export default createConfig({
   name: '__tenoxui_core__',
   entry: 'src/index.ts',
-  formats: ['es', 'iife', 'cjs']
-  /*
-  rollupOptions: {
-    external: ['@tenoxui/moxie'],
-    output: {
-      exports: 'named',
-      globals: {
-        '@tenoxui/moxie': '__tenoxui_moxie__'
-      }
-    }
-  }*/
+  formats: ['es', 'iife', 'cjs', 'umd']
 })
