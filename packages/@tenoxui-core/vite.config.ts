17a89c5a8e481f1f7442ab972372917c9c04efca
nousantx <nousantx@gmail.com>
2025-04-29 16:14:48 +0700

    feat(core): v1.4 development start

--- packages/@tenoxui-core/vite.config.ts
+++ packages/@tenoxui-core/vite.config.ts
@@ -3,7 +3,7 @@ import { createConfig } from '../vite.config.base.ts'
 export default createConfig({
   name: '__tenoxui_core__',
   entry: 'src/index.ts',
-  formats: ['es', 'iife', 'cjs'],
+  formats: ['es', 'iife', 'cjs']
   /*
   rollupOptions: {
     external: ['@tenoxui/moxie'],
