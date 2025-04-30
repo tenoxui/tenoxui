49f39be4cf8559c7d396e551f53fea04b1772fb9
nousantx <nousantx@gmail.com>
2025-05-01 06:18:03 +0700

    feat: update package.json and vite config

--- packages/tenoxui/vite.config.ts
+++ packages/tenoxui/vite.config.ts
@@ -2,9 +2,7 @@ import { createConfig } from '../vite.config.base.ts'
 
 export default createConfig({
   name: '__tenoxui_core__',
-  entry: {
-    'tenoxui-full': 'src/tenoxui-full.ts',
-    tenoxui: 'src/tenoxui.ts'
-  },
-  formats: ['es', 'iife']
+  entry: 'src/index.ts',
+  formats: ['es', 'iife', 'cjs', 'umd'],
+  sourcemap: false
 })
