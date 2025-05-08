954a18327fda10a6f307d5a311e3c33a8cf015a4
nousantx <nousantx@gmail.com>
2025-05-08 18:26:45 +0700

    feat(preset-tailwind): add typography & background utilities

--- packages/@tenoxui-preset-tailwind/src/index.ts
+++ packages/@tenoxui-preset-tailwind/src/index.ts
@@ -1,6 +1,8 @@
 import { layout } from './lib/property/layout'
 import { flexAndGrid } from './lib/property/flexbox-and-grid'
 import { sizing as sizingUtilities } from './lib/property/sizing'
+import { typography } from './lib/property/typography'
+import { background } from './lib/property/background'
 import { values } from './lib/values'
 import type { Property } from '@tenoxui/moxie'
 import type { Values, Classes } from '@tenoxui/types'
@@ -13,12 +15,15 @@ export const preset = ({ sizing = 0.25 } = {}): {
   property: {
     ...layout.property(sizing),
     ...flexAndGrid.property(sizing),
-    ...sizingUtilities(sizing)
+    ...sizingUtilities(sizing),
+    ...typography.property(sizing),
+    ...background.property(sizing)
   },
   values,
   classes: {
     ...layout.classes,
-    ...flexAndGrid.classes
+    ...flexAndGrid.classes,
+    ...typography.classes
   }
 })
 
